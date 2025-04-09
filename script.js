9a7a7798104aa5cceb4e9f30a3caeea671d8bbc3
sukumar2002 <127292893+sukumar2002@users.noreply.github.com>
2025-04-09 23:35:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -43,7 +43,6 @@ function updateTask(id) {
         }
     }
 }
-
 function deleteTask(id) {
     if (confirm('Are you sure you want to delete this task?')) {
         tasks = tasks.filter(task => task.id !== id);
